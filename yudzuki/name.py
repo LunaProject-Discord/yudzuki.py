fe16977fbf77c0d80122b29062984336fa166b0a
Midorichan <55083684+Midorichaan@users.noreply.github.com>
2021-04-29 20:30:37 +0900

    add - name

--- yudzuki/name.py
+++ yudzuki/name.py
@@ -1,1 +1,33 @@
+from datetime.datetime import utcfromtimestamp
 
+__all__ = (
+    "Name"
+)
+
+class Name:
+    
+    def __init__(self, data):
+        self.data = data
+        self._update(data)
+    
+    def __repr__(self):
+        return (
+            f"<Name name={self.name}>"
+        )
+    
+    def _update(self, data):
+        self.name = data["name"]
+        self.updated_at = data["updated_at"]
+        self.created_at = data["created_at"]
+    
+    @property
+    def name(self):
+        return self.name
+    
+    @property
+    def updated_at(self):
+        return utcfromtimestamp(self.updated_at).replace(tzinfo=datetime.timezone.utc)
+    
+    @property
+    def created_at(self):
+        return utcfromtimestamp(self.created_at).replace(tzinfo=datetime.timezone.utc)
