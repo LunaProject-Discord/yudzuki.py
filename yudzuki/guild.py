42e87d3957b34599b7c55ab34db6d501fd4a35f7
Midorichan <55083684+Midorichaan@users.noreply.github.com>
2021-04-30 00:37:25 +0900

    Update guild.py

--- yudzuki/guild.py
+++ yudzuki/guild.py
@@ -22,143 +22,139 @@ class Guild:
         return f"{self.name}#{self.discriminator}"
     
     def _update(self, data):
-        self.id = data["id"]
-        self.name = data["name"]
-        self.roles = data["roles"]
-        self.features = data["features"]
-        self.splash = data["splash"]
-        self.channels = data["channels"]
-        self.emojis = data["emojis"]
-        self.boost_tier = data["boost_tier"]
-        self.boost_count = data["boost_count"]
-        self.region = data["region"]
-        self.icon = data["icon"]
-        self.icon_url = data["icon_url"]
-        self.description = data["description"]
-        self.locale = data["locale"]
-        self.afk_timeout = data["afk_timeout"]
-        self.splash_url = data["splash_url"]
-        self.banner_url = data["banner_url"]
-        self.vanity_url = data["vanity_url"]
-        self.settings = data["settings"]
-        self.banner = data["banner"]
-        self.verification_level = data["verification_level"]
-        self.default_notification_level = data["default_notification_level"]
-        self.vanity_code = data["vanity_code"]
-        self.mfa_level = data["mfa_level"]
-        self.explicit_content_level = data["explicit_content_level"]
+        self._id = data["id"]
+        self._name = data["name"]
+        self._roles = data["roles"]
+        self._features = data["features"]
+        self._splash = data["splash"]
+        self._channels = data["channels"]
+        self._emojis = data["emojis"]
+        self._boost_tier = data["boost_tier"]
+        self._boost_count = data["boost_count"]
+        self._region = data["region"]
+        self._icon = data["icon"]
+        self._icon_url = data["icon_url"]
+        self._description = data["description"]
+        self._locale = data["locale"]
+        self._afk_timeout = data["afk_timeout"]
+        self._splash_url = data["splash_url"]
+        self._banner_url = data["banner_url"]
+        self._vanity_url = data["vanity_url"]
+        self._settings = data["settings"]
+        self._banner = data["banner"]
+        self._verification_level = data["verification_level"]
+        self._default_notification_level = data["default_notification_level"]
+        self._vanity_code = data["vanity_code"]
+        self._mfa_level = data["mfa_level"]
+        self._explicit_content_level = data["explicit_content_level"]
                         
     def _get_json(self):
         return self.data
-                       
-    @property
-    def _get_data(self):
-        return self.data
-
+                      
     @property
     def settings(self):
-        return GuildSetting(self.settings)
+        return GuildSetting(self._settings)
 
     @property
     def roles(self):
-        return [Role(v) for v in self.roles]
+        return [Role(v) for v in self._roles]
 
     @property
     def channels(self):
-        return [Channel(v) for v in self.channels]
+        return [Channel(v) for v in self._channels]
 
     @property
     def emojis(self):
-        return [Emoji(v) for v in self.emojis]                 
+        return [Emoji(v) for v in self._emojis]                 
                          
     @property
     def boost_tier(self):
-        return self.boost_tier
+        return self._boost_tier
 
     @property
     def description(self):
-        return self.description
+        return self._description
 
     @property
     def locale(self):
-        return self.locale
+        return self._locale
 
     @property
     def afk_timeout(self):
-        return self.afk_timeout
+        return self._afk_timeout
 
     @property
     def splash_url(self):
-        return self.splash_url
+        return self._splash_url
 
     @property
     def banner_url(self):
-        return self.banner_url
+        return self._banner_url
                          
     @property
     def vanity_url(self):
-        return self.vanity_url
+        return self._vanity_url
 
     @property
     def banner(self):
-        return self.banner
+        return self._banner
 
     @property
     def verification_level(self):
-        return self.verification_level
+        return self._verification_level
     
     @property
     def default_notification_level(self):
-        return self.default_notification_level
+        return self._default_notification_level
 
     @property
     def vanity_code(self):
-        return self.vanity_code
+        return self._vanity_code
 
     @property
     def mfa_level(self):
-        return self.mfa_level
+        return self._mfa_level
 
     @property
     def explicit_content_level(self):
-        return self.explicit_content_level
+        return self._explicit_content_level
 
     @property
     def region(self):
-        return self.region
+        return self._region
 
     @property
     def splash(self):
-        return self.splash
+        return self._splash
 
     @property
     def boost_count(self):
-        return self.boost_count
+        return self._boost_count
     
     @property
     def owner(self):
-        return self.owner
+        return self._owner
     
     @property
     def permissions(self):
-        return self.permissions
+        return self._permissions
    
     @property
     def icon_url(self):
-        return self.icon_url
+        return self._icon_url
     
     @property
     def icon(self):
-        return self.icon
+        return self._icon
     
     @property
     def name(self):
-        return self.name
+        return self._name
     
     @property
     def id(self):
-        return self.id
+        return self._id
     
     @property
     def features(self):
-        return self.features
+        return self._features
