d8f06ffe7d613361feb9a412e65e0551da75ba6e
Midorichan <55083684+Midorichaan@users.noreply.github.com>
2021-04-29 19:31:56 +0900

    add - staff_value

--- yudzuki/util.py
+++ yudzuki/util.py
@@ -10,3 +10,15 @@ async def json_or_text(response):
         pass
 
     return text
+
+def staff_value(value):
+    if value == 1:
+        return "staff"
+    elif value == 2:
+        return "admin"
+    elif value == 3:
+        return "subowner"
+    elif value == 4:
+        return "owner"
+    else:
+        return "default"
