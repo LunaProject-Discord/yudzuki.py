4d538aeefb83d18a8b3d0091d390506faa2bd63f
Midorichan <55083684+Midorichaan@users.noreply.github.com>
2021-04-29 20:47:56 +0900

    add - emoji cls

--- yudzuki/emoji.py
+++ yudzuki/emoji.py
@@ -1,1 +1,44 @@
+from .role import Role
 
+__all__ = (
+    "Emoji"
+)
+
+class Emoji:
+    
+    def __init__(self, data):
+        self.data = data
+        self._update(data)
+        
+    def __repr__(self):
+        return (
+            f"<Emoji name={self.name} id={self.id}>"
+        )
+    
+    def _update(self, data):
+        self.id = data["id"]
+        self.name = data["name"]
+        self.managed = data["managed"]
+        self.animated = data["animated"]
+        
+        self.roles = data.get("roles", [])
+        
+    @property
+    def id(self):
+        return self.id
+    
+    @property
+    def name(self):
+        return self.name
+    
+    @property
+    def managed(self):
+        return self.managed
+    
+    @property
+    def animated(self):
+        return self.animated
+    
+    @property
+    def roles(self):
+        return [Role(v) for v in self.roles]
