40555ec2fe28bbcc61c2fa4ba6e841cd7baea19e
Midorichan <55083684+Midorichaan@users.noreply.github.com>
2021-04-29 22:39:24 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 import setuptools
-from yudzuki import __version__
+from .yudzuki import __version__
 
 readme = ""
 with open("README.md", "r", encoding="utf-8") as f:
     readme = f.read()
 
-setuptools.setup(
-    author="Team LunaProject",
-    version=__version__,
-    url="https://github.com/LunaProject-Discord/yudzuki.py",
-    description="An API Wrapper for YudzukiAPI",
-    long_description=readme,
-    long_description_content_type="text/markdown"
-)
+try:
+    setuptools.setup(
+        author="Team LunaProject",
+        version=__version__,
+        url="https://github.com/LunaProject-Discord/yudzuki.py",
+        description="An API Wrapper for YudzukiAPI",
+        long_description=readme,
+        long_description_content_type="text/markdown"
+    )
+except Exception as e:
+    print(e)
