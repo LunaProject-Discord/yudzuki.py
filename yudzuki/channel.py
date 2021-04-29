989b9c8415367fb59533ab1b499d533bbe35ccca
Midorichan <55083684+Midorichaan@users.noreply.github.com>
2021-04-29 21:50:17 +0900

    add - いろいろ

--- yudzuki/channel.py
+++ yudzuki/channel.py
@@ -1,1 +1,33 @@
+__all__ = (
+    "TextChannel",
+    "VoiceChannel",
+    "StoreChannel",
+    "CategoryChannel",
+    "Channel"
+)
 
+class TextChannel:
+        
+    def __init__(self, data):
+        self.data = data
+    
+class VoiceChannel:
+        
+    def __init__(self, data):
+        self.data = data
+    
+class StoreChannel:
+        
+    def __init__(self, data):
+        self.data = data
+    
+class CategoryChannel:
+        
+    def __init__(self, data):
+        self.data = data
+    
+class Channel:
+    
+    def __init__(self, data):
+        self.data = data
+        
