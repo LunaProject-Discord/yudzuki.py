df25138917f2c6b119a415da3df4d90badcaeaab
Midorichan <55083684+Midorichaan@users.noreply.github.com>
2021-04-29 22:32:55 +0900

    fix - error

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import setuptools
 from yudzuki import __version__
 
+readme = ""
 with open("README.md", "r", encoding="utf-8") as f:
     readme = f.read()
 
@@ -8,7 +9,7 @@ setuptools.setup(
     name="yudzuki",
     version=__version__,
     author="Team LunaProject",
-    description="An API Wrapper for YudzukiAPI"
+    description="An API Wrapper for YudzukiAPI",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/LunaProject-Discord/yudzuki.py",
